e6176e985d08230ca657cbbb5b3ce2a57b97d825
Dirk Eddelbuettel <edd@debian.org>
2026-01-22 07:09:55 -0600

    Extend and polish demo

--- demo/chronometre.R
+++ demo/chronometre.R
@@ -1,8 +1,26 @@
 #!/usr/bin/env r
 
+stopifnot("This demo requires 'reticulate'" = requireNamespace("reticulate", quietly=TRUE))
+stopifnot("This demo requires 'RcppSpdlog'" = requireNamespace("RcppSpdlog", quietly=TRUE))
+stopifnot("This demo requires 'xptr'" = requireNamespace("xptr", quietly=TRUE))
+
 library(reticulate)
-## Python (these days ...) really wants a venv so we will use one, the default
-## value is a location I used
+## reticulate and Python in general these days really want a venv so we will use one,
+## the default value is a location used locally; if needed create one
+## check for existing virtualenv to use, or else set one up
+venvdir <- Sys.getenv("CHRONOMETRE_VENV", "/opt/venv/chronometre")
+if (dir.exists(venvdir)) {
+    use_virtualenv(venvdir, required = TRUE)
+} else {
+    ## create a virtual environment, but make it temporary
+    Sys.setenv(RETICULATE_VIRTUALENV_ROOT=tempdir())
+    virtualenv_create("r-reticulate-env")
+    virtualenv_install("r-reticulate-env", packages = c("chronometre"))
+    use_virtualenv("r-reticulate-env", required = TRUE)
+}
+ch <- import("chronometre")
+
+
 use_virtualenv(Sys.getenv("CHRONOMETRE_VENV", "/opt/venv/chronometre"))
 ch <- import("chronometre")
 
@@ -18,6 +36,8 @@ attr(sw2, "class") <- c("stopwatch", "externalptr") # class it .. and then use i
 print(sw2)                                          # so `xptr` allows us close and use
 
 sw3 <- ch$Stopwatch(  xptr::xptr_address(sw) )      # so does the Python object _with a string ctor_
-print(sw3$elapsed())                                # shows output via Python I/O
+print(sw3$elapsed())                                # shows datetime.timedelta Python formatted
+cat(sw3$count(), "\n")                              # shows double
+
 
 print(sw)                                           # object still works in R
